fa15deebf6d3e31c6a4e0b874306b7cef41c8654
Rebecca Blanton <beccaablanton@gmail.com>
2020-12-13 14:12:48 -0800

    moved files, set html routes in server.js

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
