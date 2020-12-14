4a6704a2d551e36c288dfe961c77a987a3ada30b
Rebecca Blanton <beccaablanton@gmail.com>
2020-12-14 08:09:25 -0800

    added incrementing ID to each note

--- server.js
+++ server.js
@@ -33,6 +33,21 @@ app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/assets/js/index.js"));
   });
 
+//for Deleting
+  app.get("/api/notes/:id", function(req, res) {
+    var chosen = req.params.id;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < notesDb.length; i++) {
+      if (chosen === notesDb[i].id) {
+        return res.json(notesDb[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+// posts data from note into API
   app.post("/api/notes", function(req, res) {
     var newNote = req.body;
     console.log(newNote);
@@ -43,3 +58,8 @@ app.get("/api/notes", function(req, res) {
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+  /*
+  * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+   GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+  
+  DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.*/
