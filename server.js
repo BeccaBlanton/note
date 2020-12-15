0a13e4baa3ee9db2b11e484d0c60760dc5e7fbff
Rebecca Blanton <beccaablanton@gmail.com>
2020-12-15 08:31:12 -0800

    added function in server.js to delete object when button is clicked

--- server.js
+++ server.js
@@ -34,17 +34,16 @@ app.get("/api/notes", function(req, res) {
   });
 
 //for Deleting
-  app.get("/api/notes/:id", function(req, res) {
+  app.delete("/api/notes/:id", function(req, res) {
     var chosen = req.params.id;
   
-    console.log(chosen);
-  
+    console.log("ID is "+ chosen);
+  //
     for (var i = 0; i < notesDb.length; i++) {
       if (chosen === notesDb[i].id) {
-        return res.json(notesDb[i]);
+        notesDb.splice(i,1)
       }
     }
-  
     return res.json(false);
   });
 // posts data from note into API
@@ -59,7 +58,5 @@ app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
   /*
-  * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-   GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
   
   DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.*/
